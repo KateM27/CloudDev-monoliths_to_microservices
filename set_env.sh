4b02bf93271426981d59cf39b56b28fe83b3b2b7
KateM27 <99427213+KateM27@users.noreply.github.com>
2022-09-15 11:43:40 +0300

    microservices

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=monolith
+export POSTGRES_HOST=mypsql.c2vzqqc7woqo.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::micro-monolith-svc
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=hello
+export URL=http://localhost:8100
